082738b396ef85e534e3f2ef9f86ab1dc37c3259
deans-bradley <developer@deansbrad.com>
2025-11-17 09:49:02 +0200

    Add: settings key constants and display names

---
+++ src/models/constants/core/settings.js
@@ -0,0 +1,20 @@
+/**
+ * Settings Key Constants
+ * Defines the available settings key constants and their corresponding values
+ */
+const SETTINGS_KEY = Object.freeze({
+  DEFAULT_PATH: 'default-path'
+});
+
+/**
+ * Settings Keys Display Names
+ * Human-readable names for settings keys
+ */
+const SETTINGS_KEY_NAME = Object.freeze({
+  [SETTINGS_KEY.DEFAULT_PATH]: "Default path"
+});
+
+export {
+  SETTINGS_KEY,
+  SETTINGS_KEY_NAME
+};
