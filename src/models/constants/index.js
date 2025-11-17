6051f111cf48aed7857b64360aa64c2cd63161b6
deans-bradley <developer@deansbrad.com>
2025-11-17 09:45:12 +0200

    Add: core and error constants to index.js

---
+++ src/models/constants/index.js
@@ -0,0 +1,34 @@
+// Core constants
+export {
+  SETTINGS_KEY,
+  SETTINGS_KEY_NAME
+} from './core/settings.js';
+
+// Error constants
+export {
+  BASE_ERROR_TYPE,
+  ERROR_DOMAIN,
+  BASE_ERROR_MESSAGE,
+  createErrorCode,
+  createErrorMessage
+} from './errors/baseErrors.js';
+
+export {
+  CONFIG_ERROR,
+  CONFIG_ERROR_MESSAGE
+} from './errors/configError.js';
+
+export {
+  PROFILE_ERROR,
+  PROFILE_ERROR_MESSAGE
+} from './errors/profileError.js';
+
+export {
+  WORKSPACE_ERROR,
+  WORKSPACE_ERROR_MESSAGE
+} from './errors/workspaceError.js';
+
+export {
+  PROJECT_ERROR,
+  PROJECT_ERROR_MESSAGE
+} from './errors/projectError.js';
