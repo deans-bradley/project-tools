5fbe0c674b5c578445acc7d7b327337a9ca6e579
deans-bradley <developer@deansbrad.com>
2026-01-07 13:54:02 +0200

    Add: Implement Settings class for user settings and preferences

---
+++ src/models/core/Settings.ts
@@ -0,0 +1,27 @@
+import fs from 'fs-extra';
+import { Base } from './index';
+
+/**
+ * Settings class for user settings and preferences
+ */
+class Settings extends Base {
+  defaultPath: string;
+
+  constructor(defaultPath: string) {
+    super();
+    this.defaultPath = defaultPath;
+  }
+
+  async setDefaultPath(path: string): Promise<void> {
+    try {
+      await fs.ensureDir(path);
+    } catch (error) {
+      throw error; // TODO: Throw custom Error
+    }
+    this.defaultPath = path;
+    this.touch();
+  }
+}
+
+export { Settings };
+
