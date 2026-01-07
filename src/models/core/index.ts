3149c2b21fb78e315ece8795dd64d461ecb7ca52
deans-bradley <developer@deansbrad.com>
2026-01-07 13:53:44 +0200

    Add: Export model classes from core index file

---
+++ src/models/core/index.ts
@@ -0,0 +1,7 @@
+export { Base } from './Base';
+export { Config } from './Config';
+export { Profile } from './Profile';
+export { Project } from './Project';
+export { Settings } from './Settings';
+export { Workspace } from './Workspace';
+
