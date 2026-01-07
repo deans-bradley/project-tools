3570b8ac2fd5469f857f96f3c2e6c22a9ff174b3
deans-bradley <developer@deansbrad.com>
2026-01-07 13:54:20 +0200

    Add: Implement ErrorContext class for pairing error messages with key values

---
+++ src/models/errors/ErrorContext.ts
@@ -0,0 +1,11 @@
+/**
+ * ErrorContext class for pairing error messages with key values
+ */
+class ErrorContext {
+  resource!: string;
+  field!: string;
+  operation!: string;
+}
+
+export { ErrorContext };
+
