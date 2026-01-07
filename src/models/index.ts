cdeddbe0c2e73debd60cb429f11879059c56a7f2
deans-bradley <developer@deansbrad.com>
2026-01-07 18:28:53 +0200

    Add: Create index.ts to export core and error models

---
+++ src/models/index.ts
@@ -0,0 +1,12 @@
+// Core models
+export { Base } from './core/Base';
+export { Config } from './core/Config';
+export { Profile } from './core/Profile';
+export { Project } from './core/Project';
+export { Settings } from './core/Settings';
+export { Workspace } from './core/Workspace';
+
+// Error models
+export { BusinessError } from './error/BusinessError';
+export { ConfigError } from './error/ConfigError';
+
