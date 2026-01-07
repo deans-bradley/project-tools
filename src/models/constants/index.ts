83bf702743edd0046076dbccfcba856def25be3c
deans-bradley <developer@deansbrad.com>
2026-01-07 18:29:07 +0200

    Remove: Unused error constants from index.ts to streamline exports

--- src/models/constants/index.ts
+++ src/models/constants/index.ts
@@ -7,32 +7,7 @@ export {
 } from './core/settings';
 
 // Error constants
-export {
-  BASE_ERROR_MESSAGE, BASE_ERROR_TYPE, BaseErrorMessage, BaseErrorType, createErrorCode,
-  createErrorMessage, ERROR_DOMAIN
-} from './errors/baseError';
-
-export {
-  CONFIG_ERROR,
-  CONFIG_ERROR_MESSAGE, ConfigError,
-  ConfigErrorMessage
-} from './errors/configError';
-
-export {
-  PROFILE_ERROR,
-  PROFILE_ERROR_MESSAGE, ProfileError,
-  ProfileErrorMessage
-} from './errors/profileError';
-
-export {
-  PROJECT_ERROR,
-  PROJECT_ERROR_MESSAGE, ProjectError,
-  ProjectErrorMessage
-} from './errors/projectError';
-
-export {
-  WORKSPACE_ERROR,
-  WORKSPACE_ERROR_MESSAGE, WorkspaceError,
-  WorkspaceErrorMessage
-} from './errors/workspaceError';
+export { ERROR_DOMAIN, ErrorDomain } from './error/errorDomain';
+export { createErrorMessage, ERROR_MESSAGE } from './error/errorMessage';
+export { createErrorCode, ERROR_TYPE, ErrorType } from './error/errorType';
 
