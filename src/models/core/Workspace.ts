2a6755ce163eca6256b737ded999ed10e1252c88
deans-bradley <developer@deansbrad.com>
2026-01-07 13:54:07 +0200

    Add: Implement Workspace class for development workspaces

---
+++ src/models/core/Workspace.ts
@@ -0,0 +1,23 @@
+import { generateId } from '../../util/index';
+import { Base, Project } from './index';
+
+/**
+ * Workspace class for development workspaces
+ */
+class Workspace extends Base {
+  readonly id: string;
+  name: string;
+  path: string;
+  projects: Array<Project>;
+
+  constructor(name: string, path: string, projects?: Array<Project>) {
+    super();
+    this.id = generateId('ws');
+    this.name = name;
+    this.path = path;
+    this.projects = projects || [];
+  }
+}
+
+export { Workspace };
+
