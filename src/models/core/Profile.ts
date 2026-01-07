016fbb3b2f530c100a1bccf6725b924d4e20d424
deans-bradley <developer@deansbrad.com>
2026-01-07 13:53:50 +0200

    Add: Implement Profile class for user profiles with constructor and properties

---
+++ src/models/core/Profile.ts
@@ -0,0 +1,23 @@
+import { generateId } from '../../util/index';
+import { Base, Workspace } from './index';
+
+/**
+ * Profile class for user profiles
+ */
+class Profile extends Base {
+  readonly id: string;
+  name: string;
+  isActive: boolean;
+  workspaces: Array<Workspace>;
+
+  constructor(name: string, isActive?: boolean, workspaces?: Array<Workspace>) {
+    super();
+    this.id = generateId('prof');
+    this.name = name;
+    this.isActive = isActive || false;
+    this.workspaces = workspaces || [];
+  }
+}
+
+export { Profile };
+
