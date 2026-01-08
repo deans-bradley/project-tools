9584f8cda54c250557e61d3670b76393372af239
deans-bradley <developer@deansbrad.com>
2026-01-08 12:50:06 +0200

    Update: Remove UNKOWN and GENERAL domain constants

--- src/models/constants/error/errorDomain.ts
+++ src/models/constants/error/errorDomain.ts
@@ -7,8 +7,7 @@ export const ERROR_DOMAIN = {
   SETTINGS: 200,
   PROFILE: 300,
   WORKSPACE: 400,
-  PROJECT: 500,
-  UNKOWN: 900
+  PROJECT: 500
 } as const;
 
 export type ErrorDomain = typeof ERROR_DOMAIN[keyof typeof ERROR_DOMAIN];
