d4450e1867601864f32c27aef1ef04a59749f928
deans-bradley <developer@deansbrad.com>
2026-01-08 14:33:07 +0200

    Add: InValidValueError class to handle invalid value errors

---
+++ src/models/error/InvalidValueError.ts
@@ -0,0 +1,12 @@
+import { ERROR_TYPE, ErrorDomain } from "../constants";
+import { BusinessError } from "./BusinessError";
+
+class InValidValueError extends BusinessError {
+  constructor(errorDomain: ErrorDomain, fieldName: string, fieldValue: string, message?: string) {
+    const errorMessage = message || `Value for '${fieldName}' cannot be '${fieldValue}'`;
+    super(errorDomain, ERROR_TYPE.INVALID_VALUE, errorMessage);
+  }
+}
+
+export { InValidValueError };
+
