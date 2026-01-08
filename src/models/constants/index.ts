77cfb47010c1a77a51243e77509e55fdbab4929c
deans-bradley <developer@deansbrad.com>
2026-01-08 14:30:30 +0200

    Remove: Error message imports for constants index

--- src/models/constants/index.ts
+++ src/models/constants/index.ts
@@ -8,6 +8,5 @@ export {
 
 // Error constants
 export { ERROR_DOMAIN, ErrorDomain } from './error/errorDomain';
-export { createErrorMessage, ERROR_MESSAGE } from './error/errorMessage';
 export { createErrorCode, ERROR_TYPE, ErrorType } from './error/errorType';
 
