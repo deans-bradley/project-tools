c8ee10821b5a05db1377590d8b7012fd143ecf7c
deans-bradley <developer@deansbrad.com>
2026-01-08 12:49:30 +0200

    Add: Error models to model index

--- src/models/index.ts
+++ src/models/index.ts
@@ -9,4 +9,8 @@ export { Workspace } from './core/Workspace';
 // Error models
 export { BusinessError } from './error/BusinessError';
 export { ConfigError } from './error/ConfigError';
+export { ProfileError } from './error/ProfileError';
+export { ProjectError } from './error/ProjectError';
+export { SettingsError } from './error/SettingsError';
+export { WorkspaceError } from './error/WorkspaceError';
 
