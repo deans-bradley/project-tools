31abe92a1acc73926a1dd2676d123b0025e4c68b
deans-bradley <developer@deansbrad.com>
2026-01-08 14:29:57 +0200

    Add: Error models to model index

--- src/models/index.ts
+++ src/models/index.ts
@@ -7,10 +7,18 @@ export { Settings } from './core/Settings';
 export { Workspace } from './core/Workspace';
 
 // Error models
+export { AccessToResourceDeniedError } from './error/AccessToResourceDeniedError';
+export { BackupFailedError } from './error/BackupFailedError';
 export { BusinessError } from './error/BusinessError';
-export { ConfigError } from './error/ConfigError';
-export { ProfileError } from './error/ProfileError';
-export { ProjectError } from './error/ProjectError';
-export { SettingsError } from './error/SettingsError';
-export { WorkspaceError } from './error/WorkspaceError';
+export { DiskFullError } from './error/DiskFullError';
+export { EmptyRequiredFieldError } from './error/EmptyRequiredFieldError';
+export { InitializationFailedError } from './error/InitializationFailedError';
+export { InvalidFormatError } from './error/InvalidFormatError';
+export { InValidValueError } from './error/InvalidValueError';
+export { OperationFailedError } from './error/OperationFailedError';
+export { ReadError } from './error/ReadError';
+export { ResourceAlreadyExistsError } from './error/ResourceAlreadyExistsError';
+export { ResourceNotFoundError } from './error/ResourceNotFoundError';
+export { RestoreFailedError } from './error/RestoreFailedError';
+export { WriteError } from './error/WriteError';
 
