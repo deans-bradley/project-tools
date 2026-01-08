b0af52bf368808fc495d33bf6afe12f891e870f0
deans-bradley <developer@deansbrad.com>
2026-01-08 16:04:52 +0200

    Refactor: Enhance Config class to manage profiles and improve data handling

--- src/models/core/Config.ts
+++ src/models/core/Config.ts
@@ -1,36 +1,52 @@
 import { version } from '../../../package.json';
-import { Base, Settings } from '../index';
-
-interface ConfigData {
-  appVersion?: string;
-  settings: any;
-  createdDate?: string;
-  modifiedDate?: string;
-}
+import { ERROR_DOMAIN } from '../constants';
+import { Base, Profile, ResourceNotFoundError, Settings } from '../index';
+import { ConfigData } from './interfaces/ConfigData';
 
 /**
- * Config class for app configuration
+ * Config class for app configuration and data
  */
 class Config extends Base {
   readonly appVersion: string;
   settings: Settings;
+  profiles: Profile[];
 
   constructor(settings: Settings) {
     super();
     this.appVersion = version;
     this.settings = settings;
+    this.profiles = [];
+  }
+
+  /**
+   * Add a new profile to the app config
+   * @param profile - The new profile to be added
+   */
+  addProfile(profile: Profile): void {
+    this.profiles.push(profile);
+  }
+  
+  /**
+   * Remove a profile from the app config by the profile's ID
+   * @param profileId - The profile ID of the profile to be removed
+   */
+  removeProfile(profileId: string): void {
+    const profileIndex = this.profiles.findIndex(p => p.id === profileId);
+
+    if (profileIndex === -1) {
+      throw new ResourceNotFoundError(ERROR_DOMAIN.PROFILE, `profile with ID ${profileId}`);
+    } else {
+      this.profiles.splice(profileIndex);
+    }
   }
 
   static fromJSON(data: ConfigData): Config {
     const settings = new Settings(data.settings.defaultPath);
     const config = new Config(settings);
     
-    if (data.createdDate) {
-      (config as any).createdDate = new Date(data.createdDate);
-    }
-    if (data.modifiedDate) {
-      config.modifiedDate = new Date(data.modifiedDate);
-    }
+    config.profiles = data.profiles;
+    config.createdDate = data.createdDate;
+    config.modifiedDate = data.modifiedDate;
     
     return config;
   }
@@ -41,8 +57,9 @@ class Config extends Base {
       settings: {
         defaultPath: this.settings.defaultPath
       },
-      createdDate: this.createdDate.toISOString(),
-      modifiedDate: this.modifiedDate.toISOString()
+      profiles: this.profiles,
+      createdDate: this.createdDate,
+      modifiedDate: this.modifiedDate
     };
   }
 }
