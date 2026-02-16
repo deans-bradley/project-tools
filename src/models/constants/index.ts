a229e9ac716fd4b068050bbbdad6c7b981eccdf9
deans-bradley <developer@deansbrad.com>
2026-02-16 10:38:48 +0200

    Update: Export for Settings constants

--- src/models/constants/index.ts
+++ src/models/constants/index.ts
@@ -1,12 +1,7 @@
 // Core constants
-export {
-  SETTINGS_KEY,
-  SETTINGS_KEY_NAME,
-  SettingsKey,
-  SettingsKeyName
-} from './core/settings';
+export { SETTINGS, SETTINGS_KEY } from './core/settings';
 
 // Error constants
-export { ERROR_DOMAIN, ErrorDomain } from './error/errorDomain';
+export { ERROR_DOMAIN, ErrorDomain, PROFILE_ERROR, WORKSPACE_ERROR } from './error/errorDomain';
 export { createErrorCode, ERROR_TYPE, ErrorType } from './error/errorType';
 
