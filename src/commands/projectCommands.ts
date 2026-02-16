e1555282763ca1b2fe08b79ddeff9dea25f69062
deans-bradley <developer@deansbrad.com>
2026-02-16 10:36:57 +0200

    Add: Create projectCommands.ts file with initial setup for project-related CLI commands

---
+++ src/commands/projectCommands.ts
@@ -0,0 +1,17 @@
+// import chalk from 'chalk';
+// import { Command } from 'commander';
+// import { ProjectManager } from '../managers';
+
+// /**
+//  * Setup project-related CLI commands
+//  */
+// export function setupProjectCommands(program: Command): void {
+//   const projectManager = new ProjectManager();
+
+//   const projectCommand = program
+//     .command('project')
+//     .description('Manage projects')
+//     .action(() => {
+//       console.log(chalk.yellow('Project management coming soon...'));
+//     });
+// }
