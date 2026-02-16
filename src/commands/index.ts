968c32d69275cd9f2dc7b24e1d8dcdd5cba07772
deans-bradley <developer@deansbrad.com>
2026-02-16 10:36:36 +0200

    Update: Comment out setupProjectCommands export

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,2 +1,5 @@
 export { setupConfigCommands } from './configCommands';
+export { setupProfileCommands } from './profileCommands';
+// export { setupProjectCommands } from './projectCommands';
+export { setupWorkspaceCommands } from './workspaceCommands';
 
