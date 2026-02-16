0807c5e27ab1127a4da161c677c3b638dbf222ef
deans-bradley <developer@deansbrad.com>
2026-02-16 10:38:01 +0200

    Add: Create ProjectManager class for handling project-related operations

---
+++ src/managers/ProjectManager.ts
@@ -0,0 +1,8 @@
+
+/**
+ * ProjectManager - Handles all project-related operations
+ */
+export class ProjectManager {
+  // TODO: Implement project management functionality
+  // This class is prepared for future implementation
+}
