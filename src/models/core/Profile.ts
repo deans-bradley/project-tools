71b304c5807be7c25e00008d7090b37405bb9613
deans-bradley <developer@deansbrad.com>
2026-02-16 10:39:43 +0200

    Add: Implement addWorkspace and removeWorkspace methods in Profile class

--- src/models/core/Profile.ts
+++ src/models/core/Profile.ts
@@ -17,6 +17,22 @@ class Profile extends Base {
     this.isActive = isActive || false;
     this.workspaces = workspaces || [];
   }
+
+  /**
+   * Adds a new workspace to the active profile
+   * @param workspace
+   */
+  addWorkspace(workspace: Workspace) {
+    this.workspaces.push(workspace);
+  }
+
+  /**
+   * Removes a workspace from the active profile
+   * @param workspaceName
+   */
+  removeWorkspace(workspaceName: string) {
+    this.workspaces = this.workspaces.filter(w => w.name !== workspaceName);
+  }
 }
 
 export { Profile };
